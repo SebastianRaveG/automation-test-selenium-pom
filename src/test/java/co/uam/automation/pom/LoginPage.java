0419c239712f14f6cf2d36e5452329212e4784ff
SebasrtianRaveG <seba339931@gmail.com>
2024-09-24 21:17:09 -0500

    Add the rest of points in the tests

--- src/test/java/co/uam/automation/pom/LoginPage.java
+++ src/test/java/co/uam/automation/pom/LoginPage.java
@@ -3,13 +3,19 @@ package co.uam.automation.pom;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 public class LoginPage extends BasePage {
-    public By loginOption = By.linkText("Log in");
-    public By nameofuser = By.id("nameofuser");
+    public By loginOption = By.id("login2");
     public By username = By.id("loginusername");
     public By password = By.id("loginpassword");
     public By loginButton = By.xpath("//button[text()='Log in']");
     public By searchProduct = By.xpath("//a[contains(.,'Nokia lumia 1520')]");
     public By addToCartButton = By.xpath("//a[@onclick='addToCart(2)']");
+    public By cartOption = By.linkText("Cart");
+    public By productInCart = By.xpath("//td[contains(text(),'Nokia lumia 1520')]");
+    public By placeOrderButton = By.xpath("//button[text()='Place Order']");
+    public By nameField = By.id("name");
+    public By cardField = By.id("card");
+    public By purchaseButton = By.xpath("//button[text()='Purchase']");
+    public By confirmationMessage = By.xpath("//h2[text()='Thank you for your purchase!']");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -35,5 +41,35 @@ public class LoginPage extends BasePage {
         driver.switchTo().alert().accept();
         return alertMessage;
     }
+
+    public void Cart() throws InterruptedException{
+        click(cartOption);
+        Thread.sleep(2000);
+    }
+
+    public boolean isOnCartPage() {
+        return driver.getCurrentUrl().contains("/cart.html");
+    }
+
+    public boolean isProductInCart() {
+        return isDisplayed(productInCart);
+    }
+
+    public void placeOrder() throws InterruptedException {
+        click(placeOrderButton);
+        Thread.sleep(2000);
+    }
+
+    public void fillPurchaseForm(String name, String card) throws InterruptedException {
+        type(name, nameField);
+        type(card, cardField);
+        click(purchaseButton);
+        Thread.sleep(2000);
+    }
+
+    public boolean isPurchaseSuccessful() {
+        return isDisplayed(confirmationMessage);
+    }
+
 }
 
--- src/test/java/co/uam/automation/pom/LoginTest.java
+++ src/test/java/co/uam/automation/pom/LoginTest.java
@@ -7,25 +7,35 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class LoginTest {
+
     private WebDriver driver;
     LoginPage loginPage;
+
     @BeforeMethod
     public void setUp() throws Exception{
         loginPage = new LoginPage(driver);
         driver = loginPage.chromeDriverConnection();
+        driver.manage().window().maximize();
         loginPage.visit("https://www.demoblaze.com/");
     }
+
     @AfterMethod
     public void tearDown() throws Exception{
         driver.quit();
     }
+
     @Test
     public void loginUser() throws InterruptedException {
         loginPage.login("username", "password");
         Thread.sleep(2000);
         loginPage.selectProduct();
         String alertMessage = loginPage.addProductToCart();
-        // Validar el mensaje de la alerta
         assertEquals(alertMessage, "Product added.");
+        loginPage.Cart();
+        assertTrue(loginPage.isOnCartPage(), "No se redirigió correctamente a la página del carrito.");
+        assertTrue(loginPage.isProductInCart(), "El producto no está en el carrito.");
+        loginPage.placeOrder();
+        loginPage.fillPurchaseForm("TestName", "123123123");
+        assertTrue(loginPage.isPurchaseSuccessful());
     }
 }
